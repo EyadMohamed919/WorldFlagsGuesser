19b55e4cfce83c8ca63bb2af93010d05db431e55
eydos <eyad.mo06@gmail.com>
2025-01-28 14:04:29 +0200

    Added settings button and activity

--- app/src/main/java/com/eydosentertainment/worldflagsguesser/MainActivity.java
+++ app/src/main/java/com/eydosentertainment/worldflagsguesser/MainActivity.java
@@ -3,15 +3,17 @@ package com.eydosentertainment.worldflagsguesser;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton playButton;
     private ImageButton studyButton;
+    private FloatingActionButton settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,10 +36,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void assignViews(){
         playButton = findViewById(R.id.PlayBtn);
-        studyButton = findViewById(R.id.StudyBtn);
+        studyButton = findViewById(R.id.SettingsReturn);
+        settingsButton = findViewById(R.id.SettingsButton);
     }
 }
