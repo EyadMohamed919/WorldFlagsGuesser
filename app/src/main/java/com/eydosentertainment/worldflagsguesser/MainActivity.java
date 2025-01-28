30184e90d68c23c499bb5dc8506612bedcc9c08e
eydos <eyad.mo06@gmail.com>
2025-01-28 13:21:35 +0200

    Added Flags, retrive flags functionality, and scoring functions

--- app/src/main/java/com/eydosentertainment/worldflagsguesser/MainActivity.java
+++ app/src/main/java/com/eydosentertainment/worldflagsguesser/MainActivity.java
@@ -1,19 +1,43 @@
 package com.eydosentertainment.worldflagsguesser;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton playButton;
+    private ImageButton studyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        assignViews();
 
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GuessingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        studyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    public void assignViews(){
+        playButton = findViewById(R.id.PlayBtn);
+        studyButton = findViewById(R.id.StudyBtn);
     }
 }
